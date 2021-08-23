9ed55c99fe2f7f43cf57c0b1cd05746330b5e290
Ameen Assadi <ameen25144@gmail.com>
2021-08-23 16:13:49 +0300

    bug-fixing

--- add2cart.php
+++ add2cart.php
@@ -7,7 +7,7 @@
 		$userID=$_SESSION['id'];
 		$id = $_POST['id'];
 		$qnt = (int)explode('=',$_POST['qnt'])[1];
-		$sql = "SELECT * FROM items WHERE Barcode = ?";
+		$sql = "SELECT * FROM items WHERE Barcode = ? LIMIT 1;";
 		$stmt= mysqli_stmt_init($conn);
 		mysqli_stmt_prepare($stmt,$sql);
 		mysqli_stmt_bind_param($stmt,"i",$id);
@@ -19,14 +19,18 @@
 		$img = $row['img'];
 		$depNum = $row['Department'];
 		//Checking if the product already exists into cart update quantity only
-		$sql = "SELECT itemBarcode from Cart WHERE userID = ?;";
+		$sql = "SELECT * from Cart WHERE userID = ? AND itemBarcode =?;";
 		$stmt = mysqli_stmt_init($conn);
 		mysqli_stmt_prepare($stmt,$sql);
-		mysqli_stmt_bind_param($stmt,"i",$userID);
+		mysqli_stmt_bind_param($stmt,"ii",$userID,$id);
 		mysqli_stmt_execute($stmt);
 		$res = mysqli_stmt_get_result($stmt);
 		if(mysqli_num_rows($res)>0)
 		{
+			while($row = mysqli_fetch_assoc($res))
+			{
+			$itemBarcode = $row['itemBarcode'];
+			if($itemBarcode ==$id){
 			$sql = "SELECT qnt FROM cart WHERE itemBarcode = ? AND userID = ?;";
 			$stmt = mysqli_stmt_init($conn);
 			mysqli_stmt_prepare($stmt,$sql);
@@ -41,7 +45,8 @@
 			mysqli_stmt_prepare($stmt,$sql);
 			mysqli_stmt_bind_param($stmt,"iii",$newQ,$id,$userID);
 			mysqli_stmt_execute($stmt);
-
+			}
+		}
 		}
 		else {
 		$sql = "INSERT INTO cart (userID,name,price,qnt,itemBarcode,depNum,img) VALUES (?,?,?,?,?,?,?);";
