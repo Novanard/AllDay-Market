829a69d8362fe9216f4d18a7f7fe5deb39719ee3
Ameen Assadi <ameen25144@gmail.com>
2021-09-03 09:48:53 +0300

    databasebackup

--- allday.sql
+++ allday.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 25, 2021 at 02:04 AM
+-- Generation Time: Sep 03, 2021 at 08:48 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.26
 
@@ -35,19 +35,9 @@ CREATE TABLE `cart` (
   `qnt` int(11) NOT NULL,
   `itemBarcode` int(11) NOT NULL,
   `depNum` tinyint(1) NOT NULL,
+  `checkedOut` tinyint(1) DEFAULT 0,
   `img` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`id`, `userID`, `name`, `price`, `qnt`, `itemBarcode`, `depNum`, `img`) VALUES
-(24, 2, 'Arabian Pita', 10, 400, 31, 3, 'assets/images/items/Pita.jpg'),
-(31, 5, 'Sinta Meat', 110, 4, 41, 4, 'assets/images/items/sinta.png'),
-(32, 5, 'Laundry Machine', 1200, 1, 21, 2, 'assets/images/items/ghsale.jpg'),
-(33, 5, 'Tomatoes', 4, 2, 11, 1, 'assets/images/items/bandora.jpg'),
-(34, 6, 'Small Heater', 600, 3, 22, 2, 'assets/images/items/sheater.jpg');
 
 -- --------------------------------------------------------
 
@@ -66,9 +56,9 @@ CREATE TABLE `departments` (
 
 INSERT INTO `departments` (`Num`, `Name`) VALUES
 (1, 'Vegehtables'),
-(2, 'Butchery'),
+(2, 'HomeTools'),
 (3, 'Bakery'),
-(4, 'HomeTools');
+(4, 'Butchery');
 
 -- --------------------------------------------------------
 
@@ -93,8 +83,9 @@ CREATE TABLE `employees` (
 --
 
 INSERT INTO `employees` (`id`, `eID`, `PIN`, `firstname`, `lastname`, `depNum`, `perhour`, `residence`, `avatar`) VALUES
-(1, 322470303, '2109', 'Ameen', 'Assadi', 1, 60, 'Deir El Assad', 'assets/images/employees/hey, don\'t worry_.jpg'),
-(5, 322470305, '2109', 'AAAA', 'SSSSS', 1, 500, 'Test', 'assets/images/employees/noPic.jpg');
+(1, 322470303, '2109', 'Ameen', 'Assadi', 3, 60, 'Deir El Assad', 'assets/images/employees/hey, don\'t worry_.jpg'),
+(5, 322470305, '2109', 'AAAA', 'SSSSS', 1, 500, 'Test', 'assets/images/employees/noPic.jpg'),
+(6, 123456, '1234', 'TEST', 'test', 1, 50, 'Test', 'assets/images/employees/noPic.jpg');
 
 -- --------------------------------------------------------
 
@@ -118,19 +109,19 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`Barcode`, `Name`, `Price`, `Department`, `img`, `quantity`, `supplierID`, `sellCount`) VALUES
-(11, 'Tomatoes', 4, 1, 'assets/images/items/bandora.jpg', 186, 0, 23),
-(12, 'Cucmber', 4, 1, 'assets/images/items/khear.jpg', 196, 0, 14),
-(13, 'Garlic', 4, 1, 'assets/images/items/garlic.png', 197, 0, 3),
-(14, 'ChilliPepper', 5, 1, 'assets/images/items/pepper.png', 182, 0, 15),
-(21, 'Laundry Machine', 1200, 2, 'assets/images/items/ghsale.jpg', 193, 0, 16),
-(22, 'Small Heater', 600, 2, 'assets/images/items/sheater.jpg', 197, 0, 4),
+(11, 'Tomatoes', 4, 1, 'assets/images/items/bandora.jpg', 154, 0, 55),
+(12, 'Cucmber', 4, 1, 'assets/images/items/khear.jpg', 0, 0, 204),
+(13, 'Garlic', 4, 1, 'assets/images/items/garlic.png', 193, 0, 7),
+(14, 'ChilliPepper', 5, 1, 'assets/images/items/pepper.png', 178, 0, 19),
+(21, 'Laundry Machine', 1200, 2, 'assets/images/items/ghsale.jpg', 177, 0, 32),
+(22, 'Small Heater', 600, 2, 'assets/images/items/sheater.jpg', 190, 0, 11),
 (23, 'Radiator', 800, 2, 'assets/images/items/heater.jpg', 200, 0, 3),
 (24, 'Vaccum Cleaner', 450, 2, 'assets/images/items/sho2ev.jpg', 196, 0, 6),
 (31, 'Arabian Pita', 10, 3, 'assets/images/items/Pita.jpg', 0, 0, 200),
-(33, 'Oreo Cake', 150, 3, 'assets/images/items/oreo.jpg', 197, 0, 3),
+(33, 'Oreo Cake', 150, 3, 'assets/images/items/oreo.jpg', 0, 0, 193),
 (34, 'French Waffles', 30, 3, 'assets/images/items/waffle.jpg', 200, 0, 0),
-(41, 'Sinta Meat', 110, 4, 'assets/images/items/sinta.png', 188, 0, 12),
-(42, 'Tomhawk Steak', 300, 4, 'assets/images/items/Tomahawk.jpg', 168, 0, 32),
+(41, 'Sinta Meat', 110, 4, 'assets/images/items/sinta.png', 186, 0, 14),
+(42, 'Tomhawk Steak', 300, 4, 'assets/images/items/Tomahawk.jpg', 133, 0, 67),
 (43, 'Sheep Shoulder', 45, 4, 'assets/images/items/shoulder1.jpg', 200, 0, 0),
 (44, 'Entrecote Steak ', 50, 4, 'assets/images/items/en2.jpg', 198, 0, 2),
 (52, 'Jewish Bread', 4, 3, 'assets/images/items/bread.jpg', 200, 0, 0);
@@ -189,8 +180,7 @@ CREATE TABLE `oldpayroll_details` (
 --
 
 INSERT INTO `oldpayroll_details` (`id`, `startTime`, `endTime`, `totalTime`, `payday`, `payroll_id`) VALUES
-(46, '2021-09-23 15:38:30.000000', '2021-09-23 15:38:50.000000', 0, 40, 55),
-(48, '2021-10-23 15:42:22.000000', '2021-10-23 15:42:55.000000', 1, 66, 57);
+(1, '2021-09-01 11:35:34.000000', '2021-09-01 11:36:28.000000', 100, 108, 1);
 
 -- --------------------------------------------------------
 
@@ -212,8 +202,7 @@ CREATE TABLE `oldpayroll_ids` (
 --
 
 INSERT INTO `oldpayroll_ids` (`id`, `eID`, `payMonth`, `totalTime`, `totalMoney`, `isFinished`) VALUES
-(55, 322470303, '2021-09-23', 0, 40, 1),
-(57, 322470303, '2021-10-23', 1, 66, 1);
+(1, 322470303, '2021-09-01', 100, 1000, 1);
 
 -- --------------------------------------------------------
 
@@ -229,15 +218,6 @@ CREATE TABLE `orders_id` (
   `totalMoney` int(11) DEFAULT NULL,
   `isDone` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `orders_id`
---
-
-INSERT INTO `orders_id` (`id`, `userID`, `date`, `totalItems`, `totalMoney`, `isDone`) VALUES
-(57, 5, '2021-08-24 22:06:25', 1, 440, 0),
-(58, 5, '2021-08-24 22:06:47', 2, 1640, 0),
-(59, 6, '2021-08-24 22:35:09', 1, 1800, 0);
 
 -- --------------------------------------------------------
 
@@ -257,16 +237,6 @@ CREATE TABLE `order_details` (
   `isDone` tinyint(1) DEFAULT 0,
   `img` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `order_details`
---
-
-INSERT INTO `order_details` (`id`, `itemBarcode`, `itemName`, `depNum`, `price`, `quantity`, `total`, `order_id`, `isDone`, `img`) VALUES
-(61, 41, 'Sinta Meat', 4, 110, 4, 440, 57, 0, 'assets/images/items/sinta.png'),
-(62, 41, 'Sinta Meat', 4, 110, 4, 440, 58, 0, 'assets/images/items/sinta.png'),
-(63, 21, 'Laundry Machine', 2, 1200, 1, 1200, 58, 0, 'assets/images/items/ghsale.jpg'),
-(64, 22, 'Small Heater', 2, 600, 3, 1800, 59, 0, 'assets/images/items/sheater.jpg');
 
 -- --------------------------------------------------------
 
@@ -289,21 +259,8 @@ CREATE TABLE `payroll_details` (
 --
 
 INSERT INTO `payroll_details` (`id`, `startTime`, `endTime`, `totalTime`, `payday`, `payroll_id`, `eID`) VALUES
-(49, '2021-10-23 15:42:22.000000', '2021-10-23 15:42:55.000000', 1, 66, 58, 322470303),
-(50, '2021-08-24 13:05:02.000000', '2021-08-24 13:05:14.000000', 0, 24, 58, 322470303),
-(51, '2021-08-24 19:54:41.000000', '2021-08-24 19:54:46.000000', 0, 10, 58, 322470303),
-(52, '2021-08-24 20:00:23.000000', '2021-08-24 20:14:38.000000', 28, 1710, 58, 322470303),
-(53, '2021-08-24 20:24:39.000000', '2021-08-24 20:24:51.000000', 0, 24, 58, 322470303),
-(54, '2021-08-24 20:27:47.000000', '2021-08-24 20:28:19.000000', 1, 64, 58, 322470303),
-(55, '2021-08-24 20:28:40.000000', '2021-08-24 20:28:51.000000', 0, 22, 58, 322470303),
-(57, '2021-08-24 20:42:30.000000', '2021-08-24 20:43:13.000000', 1, 86, 58, 322470303),
-(59, '2021-08-24 20:46:21.000000', '2021-08-24 20:48:17.000000', 3, 232, 58, 322470303),
-(60, '2021-08-24 20:48:22.000000', '2021-08-24 20:48:43.000000', 0, 42, 58, 322470303),
-(61, '2021-08-24 20:52:45.000000', '2021-08-24 20:55:26.000000', 5, 322, 58, 322470303),
-(66, '2021-08-24 21:12:40.000000', '2021-08-24 21:14:15.000000', 3, 190, 58, 322470303),
-(67, '2021-08-24 21:14:24.000000', '2021-08-24 21:14:36.000000', 0, 24, 58, 322470303),
-(68, '2021-08-24 21:26:07.000000', '2021-08-24 21:29:24.000000', 6, 3283, 59, 322470305),
-(69, '2021-08-24 21:29:54.000000', '2021-08-24 21:29:59.000000', 0, 10, 58, 322470303);
+(2, '2021-10-01 11:37:34.000000', '2021-10-01 11:37:49.000000', 20, 0, 2, 322470303),
+(3, '2021-10-01 12:15:19.000000', '2021-10-01 12:15:47.000000', 26, 250, 2, 322470303);
 
 -- --------------------------------------------------------
 
@@ -325,8 +282,7 @@ CREATE TABLE `payroll_ids` (
 --
 
 INSERT INTO `payroll_ids` (`id`, `eID`, `payMonth`, `totalTime`, `totalMoney`, `isFinished`) VALUES
-(58, 322470303, '2021-08-24', 15, 25010, 0),
-(59, 322470305, '2021-08-25', 6, 123445, 0);
+(2, 322470303, '2021-10-01', 100, 2000, 0);
 
 -- --------------------------------------------------------
 
@@ -341,28 +297,6 @@ CREATE TABLE `shift` (
   `endTime` timestamp(6) NULL DEFAULT NULL,
   `isWeekend` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `shift`
---
-
-INSERT INTO `shift` (`id`, `eID`, `startTime`, `endTime`, `isWeekend`) VALUES
-(77, 322470303, '2021-08-24 21:46:00.000000', NULL, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `suppliers`
---
-
-CREATE TABLE `suppliers` (
-  `id` int(11) NOT NULL,
-  `sID` int(11) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  `company` varchar(256) NOT NULL,
-  `phone` int(15) NOT NULL,
-  `avatar` varchar(256) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -391,7 +325,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `userType`, `address`, `number`, `avatar`, `weeklyOrders`, `lifetimeOrders`, `weeklySpent`, `lifetimeSpent`) VALUES
 (1, 'Admin', 'admin@allday.com', '$2y$10$cPByNDa2Y9rRx.ZIUlsBjeKjb5f0jGHNxiqWsmT9c1hmViNOpn6/C', 1, 'N/A', 0, 'assets/images/users/noPic.jpg', 0, 0, 0, 0),
-(5, 'Ameen Ass', 'ameen@test.com', '$2y$10$xA1TMXUo2/jt2hDslGL2hudJmGplEdlvhp0.gd1yc0sMwaLuDgt0W', 0, 'Salah El ', 506663914, 'assets/images/users/noPic.jpg', 2, 10, 2080, 6000),
+(5, 'Ameen Ass', 'ameen@test.com', '$2y$10$xA1TMXUo2/jt2hDslGL2hudJmGplEdlvhp0.gd1yc0sMwaLuDgt0W', 0, 'Salah El ', 506663914, 'assets/images/users/hey, don\'t worry_.jpg', 17, 25, 65624, 69544),
 (6, 'Test', 'test@test.com', '$2y$10$/GMfYhPDIW0Ypt1merWEFuuAqmkFXqSv7UybifJiWPrFpf.XrtDG6', 0, 'Test', 123456, 'assets/images/users/noPic.jpg', 5, 5, 3000, 3000);
 
 --
@@ -403,9 +337,9 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `userType`, `address`, `
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `userID_fk` (`userID`) USING BTREE,
   ADD KEY `itemBarcode` (`itemBarcode`),
-  ADD KEY `depNum` (`depNum`);
+  ADD KEY `depNum` (`depNum`),
+  ADD KEY `cart_ibfk_3` (`userID`);
 
 --
 -- Indexes for table `departments`
@@ -498,12 +432,6 @@ ALTER TABLE `shift`
   ADD UNIQUE KEY `shift_ibfk_1` (`eID`) USING BTREE;
 
 --
--- Indexes for table `suppliers`
---
-ALTER TABLE `suppliers`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -517,13 +445,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `items`
@@ -535,37 +463,31 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `orders_id`
 --
 ALTER TABLE `orders_id`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `payroll_details`
 --
 ALTER TABLE `payroll_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `payroll_ids`
 --
 ALTER TABLE `payroll_ids`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `shift`
 --
 ALTER TABLE `shift`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
-
---
--- AUTO_INCREMENT for table `suppliers`
---
-ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=114;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -582,7 +504,8 @@ ALTER TABLE `users`
 --
 ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`itemBarcode`) REFERENCES `items` (`Barcode`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`depNum`) REFERENCES `departments` (`Num`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`depNum`) REFERENCES `departments` (`Num`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_3` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `employees`
